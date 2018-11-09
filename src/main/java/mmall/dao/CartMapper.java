ed33dc42d98f66c99e53934905017a762b17c371
zc_glb <zcglbgood@zc.com>
2018-11-09 17:31:57 +0800

    first

---
+++ src/main/java/mmall/dao/CartMapper.java
@@ -0,0 +1,18 @@
+package mmall.dao;
+
+
+import mmall.model.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
